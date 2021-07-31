6e2824e1119946b53941c4a7e88001cc40467e06
Jamulya <65002206+Jamulya@users.noreply.github.com>
2021-07-31 04:50:42 -0600

    Добавила dz5

---
+++ dz_5/main.js
@@ -0,0 +1,74 @@
+//1
+// function Task(proritet, description, taskday) {
+//   this.proritet = proritet;
+//   this.description = description;
+//   this.taskday = taskday;
+// }
+// console.log(1, "Learn JS", 30);
+
+//2
+// var tasks = [
+    
+//  new Task("1", "Learn JS", "10"),
+//  new Task("5", "Learn React JS", "20"),
+//  new Task("10", "Learn Vue JS", "30"),
+// ]
+
+// for (const task of tasks) {
+//   console.log(task);
+// }
+
+//3
+// var tasks = ['proritet', 'description', 'taskday'];
+// var taskLengths = tasks.map(function(task) {
+//   return task.length;
+// });
+// console.log(taskLengths);
+
+// var newTasks = tasks.map(function(task) {
+//   return task.newTask;
+// })
+// console.log(tasks.newTask);
+
+//3
+var tasks = [
+  {proritet: '1', description: 'Learn JS', taskday: '10'},
+  {proritet: '5', description: 'Learn React JS', taskday: '20'},
+  {proritet: '10', description: 'Learn Vue JS', taskday: '30'},
+]
+// var proritets = tasks.map(function(task) {
+//   return task.proritet;
+// });
+// console.log(proritets);
+
+//4
+// var newTask = tasks.filter(tasks => {
+//   if (tasks.taskday >= 1) {
+//     return true
+//   }
+// })
+// console.log(newTask);
+
+//5
+// var Tasks = Tasks.reduce((prev, curr)  => {
+//   return Math.max(prev, curr)
+// });
+// var MinNum = Tasks.reduce((prev, curr) => {
+// return Math.min(prev, curr)
+// });
+// console.log(Tasks);
+// console.log(MinNum);
+
+// var proritet = tasks.reduce(function(proritet, current) {
+//   return proritet.Math.max > current.Max.task ? proritet : current;
+// },)
+// console.log(proritet);
+
+
+
+
+
+
+
+
+
